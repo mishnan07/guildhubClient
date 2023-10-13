2303f2ed0e8d38621a34aa716ee868a64277738f
mishnan <mishnanc@gmail.com>
2023-10-13 23:50:10 +0530

    beforehost

--- src/PageClients/Professionals/HirePage.jsx
+++ src/PageClients/Professionals/HirePage.jsx
@@ -1,13 +1,8 @@
 import React, { useEffect, useState } from "react";
-import Hire from '../../components/clients/hire/Hire';
-import Navbar from '../../components/clients/navbar/Navbar';
-import userAxios from "../../Axios/UserAxios";
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import HireOptions from "../../components/clients/hire/HireOptions";
-import RequirementShow from "../../components/clients/requirement/RequirementShow";
-import ProCards from "../../components/professionals/proCards/ProCards";
-import ProsCard from "../../components/professionals/proCards/ProsCard";
+
+import HireOptions from "../../Component/ClientComponent/HIrePros/HireOptions";
+
+import ProsCard from "../../Component/ClientComponent/ProList/ProsCard";
 
 const HirePage = ({Type}) => {
     const [show, setShow] = useState(false);
