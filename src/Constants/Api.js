c1c1136e33673193e94cb43acd58b356ad8a2b63
mishnan <mishnanc@gmail.com>
2023-10-14 14:24:53 +0530

    hostedd

--- src/Constants/Api.js
+++ src/Constants/Api.js
@@ -1,7 +1,7 @@
-// export const userAPI = "https://guildhub.site/";
-// export const adminAPI = 'https://guildhub.site/admin/'
-// export const professionalAPI = 'https://guildhub.site/professional/'
+export const userAPI = "https://guildhub.site/";
+export const adminAPI = 'https://guildhub.site/admin/'
+export const professionalAPI = 'https://guildhub.site/professional/'
 
-export const userAPI = "http://localhost:3000/";
-export const adminAPI = 'http://localhost:3000/admin/'
-export const professionalAPI = 'http://localhost:3000/professional/'
+// export const userAPI = "http://localhost:3000/";
+// export const adminAPI = 'http://localhost:3000/admin/'
+// export const professionalAPI = 'http://localhost:3000/professional/'
